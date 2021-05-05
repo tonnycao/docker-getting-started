a3b3fb5a099e3e1af43b62b14f774a29e9ec6654
caojian <647812411@qq.com>
2021-05-05 16:12:33 +0800

    init

---
+++ python_flask/app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/5/4 1:56 下午
+# @Author : tonnycao
+# @Email : jian860129@126.com
+# @File : app.py
+# @Project : getting-started
+
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return 'Hello World! 该页面已被访问 {} 次。\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
